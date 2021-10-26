657b1d4b8e1500f3f855444d058524424ee50637
IEHIQ <53015422+IEHIQ@users.noreply.github.com>
2021-10-26 16:18:34 +0300

    Create vector3.c
    Implemented definition for 3d vector and implementation of basic functions for working on a 3d vector
    Related to #1

---
+++ vector3.c
@@ -0,0 +1,47 @@
+typedef struct
+{
+    float x, y, z;
+} vector3;
+
+vector3 sum(vector3 *a, vector3 *b) 
+{
+    vector3 result = {
+        a->x + b->x,
+        a->y + b->y,
+        a->z + b->z,
+    };
+
+    return result;
+}
+
+vector3 sub(vector3 *a, vector3 *b)
+{
+    vector3 result = {
+        a->x - b->x,
+        a->y - b->y,
+        a->z - b->z,
+    };
+
+    return result;
+}
+
+float dot(vector3 *a, vector3 *b) 
+{
+    return a->x * b->x +
+           a->y * b->y +
+           a->z * b->z;
+}
+
+vector3 cross(vector3 *a, vector3 *b)
+{
+    float xa = a->x, ya = a->y, za = a->z;
+    float xb = b->x, yb = b->y, zb = b->z;
+
+    vector3 result = {
+        ya * zb - yb * za,
+        -(xa * zb - xb * za),
+        xa * yb - xb * ya
+    };
+
+    return result;
+}
